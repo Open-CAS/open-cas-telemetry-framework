3b35171f53566cd1ff20be064564354ad14c2073
Mariusz Barczak <mariusz.barczak@intel.com>
2019-05-27 04:39:09 -0400

    Includes changes after review
    Signed-off-by: Mariusz Barczak <mariusz.barczak@intel.com>

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -59,13 +59,15 @@ function detect_distribution ()
 
 function setup_protocol_buffer
 {
+    local build_dir="$(dirname $0)/octf-build"
+
     info "Setup google protocol buffer"
-    rm -rf build/protobuf
-    mkdir -p build
-    pushd build
+    rm -rf "${build_dir}"
+    mkdir -p "${build_dir}"
+    pushd "${build_dir}"
 
     git clone https://github.com/protocolbuffers/protobuf.git
-    check_result $? "Cannot clone google protocol buffer gtest gtest-devel"
+    check_result $? "Cannot clone google protocol buffer"
     pushd protobuf
 
     latest_release=$(curl --silent "https://github.com/protocolbuffers/protobuf/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#')
@@ -89,7 +91,7 @@ function setup_protocol_buffer
     popd
     popd
 
-    rm -rf build/protobuf
+    rm -rf "${build_dir}"
 }
 
 function setup_rhel7 ()
@@ -119,35 +121,40 @@ function setup_rhel7 ()
 
 function setup_ubuntu18 ()
 {
+    local build_dir="$(dirname $0)/octf-build/gtest"
+
     info "Setup google test"
-    rm -rf build/gtest
-    mkdir -p build/gtest
-    pushd build/gtest
+
+    rm -rf local "${build_dir}"
+    mkdir -p "${build_dir}"
+
+    pushd "${build_dir}"
     cmake /usr/src/gtest && \
     make -j$(nproc) && \
     make install -j$(nproc)
     check_result $? "Cannot setup google test"
+
+    rm -rf "${build_dir}"
 }
 
-DISTRO=$(detect_distribution)
-PACKAGES=""
-INSTALLER=""
-DISTRO_SETUP=""
+distro=$(detect_distribution)
+packages=""
+installer=""
+distro_setup=""
 
-case ${DISTRO} in
+case "${distro}" in
 "RHEL7")
     info "RHEL7.x/CentOS7.x detected"
-    PACKAGES="cmake autoconf automake libtool
-        curl make gcc-c++ unzip"
-    INSTALLER="yum"
-    DISTRO_SETUP=setup_rhel7
+    packages="cmake autoconf automake libtool curl make gcc-c++ unzip"
+    installer="yum"
+    distro_setup=setup_rhel7
     ;;
 "UBUNTU18")
     info "Ubuntu 18 detected"
-    PACKAGES="cmake autoconf automake libtool curl make g++ unzip
+    packages="cmake autoconf automake libtool curl make g++ unzip
         libprotobuf-dev protobuf-compiler libgtest-dev"
-    INSTALLER="apt-get"
-    DISTRO_SETUP=setup_ubuntu18
+    installer="apt-get"
+    distro_setup=setup_ubuntu18
     ;;
 *)
     error "Unknown linux distribution"
@@ -155,8 +162,8 @@ case ${DISTRO} in
     ;;
 esac
 
-info "Install pacakges: " ${PACKAGES}
-${INSTALLER} -y install ${PACKAGES}
+info "Install pacakges: ${packages}"
+${installer} -y install ${packages}
 check_result $? "Cannot install required dependencies"
 
-${DISTRO_SETUP}
+${distro_setup}
