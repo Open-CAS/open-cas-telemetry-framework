8ceb64b442d8ba15abbf55e9777e7cc590a841ef
Mariusz Barczak <mariusz.barczak@intel.com>
2019-07-17 15:51:56 +0200

    Switches IO parser to present by default parsed IO

--- source/octf/interface/InterfaceTraceParsingImpl.cpp
+++ source/octf/interface/InterfaceTraceParsingImpl.cpp
@@ -5,11 +5,63 @@
 
 #include <octf/interface/InterfaceTraceParsingImpl.h>
 
+#include <octf/trace/parser/IoTraceEventHandler.h>
 #include <octf/trace/parser/IoTraceEventHandlerCsvPriner.h>
 #include <octf/trace/parser/IoTraceEventHandlerJsonPrinter.h>
 #include <octf/utils/Exception.h>
 
 namespace octf {
+
+class IoTraceEventHandlerPrinter : public IoTraceEventHandler {
+public:
+    IoTraceEventHandlerPrinter(const std::string &tracePath,
+                               proto::OutputFormat format)
+            : IoTraceEventHandler(tracePath)
+            , m_table()
+            , m_format(format)
+            , m_jsonOptions()
+            , m_jsonTrace() {
+        m_jsonOptions.always_print_primitive_fields = true;
+    }
+
+    virtual ~IoTraceEventHandlerPrinter() = default;
+
+    void handleIO(proto::trace::ParsedEvent &io) override {
+        switch (m_format) {
+        case proto::OutputFormat::CSV: {
+            m_table[0].clear();
+            m_table[0] << io;
+            std::cout << m_table << std::endl;
+        } break;
+
+        case proto::OutputFormat::JSON: {
+            m_jsonTrace.clear();
+            google::protobuf::util::MessageToJsonString(io, &m_jsonTrace,
+                                                        m_jsonOptions);
+            std::cout << m_jsonTrace << std::endl;
+        } break;
+
+        default: { throw Exception("Invalid output format"); } break;
+        }
+    }
+
+    void processEvents() override {
+        if (m_format == proto::OutputFormat::CSV) {
+            // Form CSV header and print it
+            proto::trace::ParsedEvent event;
+            table::setHeader(m_table[0], &event);
+            std::cout << m_table << std::endl;
+        }
+
+        TraceEventHandler<proto::trace::Event>::processEvents();
+    }
+
+private:
+    table::Table m_table;
+    proto::OutputFormat m_format;
+    google::protobuf::util::JsonOptions m_jsonOptions;
+    std::string m_jsonTrace;
+};
 
 void InterfaceTraceParsingImpl::ParseTrace(
         ::google::protobuf::RpcController *controller,
@@ -19,14 +71,20 @@ void InterfaceTraceParsingImpl::ParseTrace(
     (void) (response);
 
     try {
-        if (request->format() == proto::OutputFormat::JSON) {
-            IoTraceEventHandlerJsonPrinter parser(request->tracepath());
-            parser.processEvents();
-        } else if (request->format() == proto::OutputFormat::CSV) {
-            IoTraceEventHandlerCsvPrinter parser(request->tracepath());
-            parser.processEvents();
+        if (request->raw()) {
+            if (request->format() == proto::OutputFormat::JSON) {
+                IoTraceEventHandlerJsonPrinter parser(request->tracepath());
+                parser.processEvents();
+            } else if (request->format() == proto::OutputFormat::CSV) {
+                IoTraceEventHandlerCsvPrinter parser(request->tracepath());
+                parser.processEvents();
+            } else {
+                throw Exception("Invalid output format");
+            }
         } else {
-            throw Exception("Invalid output format");
+            IoTraceEventHandlerPrinter parser(request->tracepath(),
+                                              request->format());
+            parser.processEvents();
         }
     } catch (const Exception &ex) {
         controller->SetFailed(ex.what());
