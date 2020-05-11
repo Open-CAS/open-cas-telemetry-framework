0e903e53f6c022f81e6ca58ffe2438d6156a3546
mmkayPL <mkmedipl@gmail.com>
2020-05-11 09:25:24 +0200

    Update octf-common.sh

--- tools/scripts/octf-common.sh
+++ tools/scripts/octf-common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright(c) 2012-2018 Intel Corporation
+# Copyright(c) 2012-2020 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
 OCTF_ROOT_DIR="$(cd $(dirname ${BASH_SOURCE[0]})/../../ && pwd)"
