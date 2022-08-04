d9356aa06f344881868cd61aa8de7eddff0e2331
Mariusz Barczak <mariusz.barczak@intel.com>
2022-08-04 07:00:44 -0400

    Updates the interface for trace extensions builders
    Signed-off-by: Mariusz Barczak <mariusz.barczak@intel.com>

--- source/octf/interface/ITraceExtensionBuilder.h
+++ source/octf/interface/ITraceExtensionBuilder.h
@@ -3,37 +3,78 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef SOURCE_OCTF_INTERFACE_ITRACEXTENSIONBUILDER_H
-#define SOURCE_OCTF_INTERFACE_ITRACEXTENSIONBUILDER_H
+#ifndef SOURCE_OCTF_INTERFACE_ITRACEEXTENSIONBUILDER_H
+#define SOURCE_OCTF_INTERFACE_ITRACEEXTENSIONBUILDER_H
 
 #include <google/protobuf/message.h>
-#include <cstdint>
+#include <functional>
+#include <list>
 #include <octf/interface/ITraceConverter.h>
 #include <octf/proto/parsedTrace.pb.h>
+#include <octf/utils/Types.h>
 #include <octf/utils/table/Table.h>
 
 namespace octf {
 
 /**
- * @brief The implementation of this interface should be able
- * to build extensions based on OCTF data
+ * @brief The interface which should be able to build an trace extensions for
+ * the specific trace.
+ *
+ * @tparam TraceEvent The message prototype of trace for which the extension is
+ * produced
  */
+template <typename TraceEvent>
 class ITraceExtensionBuilder {
 public:
     virtual ~ITraceExtensionBuilder() = default;
+
     /**
-     * @brief A specific implementation of the builder will build
-     * extension and return it in the table
+     * @brief Gets the name of trace extension
+     *
+     * @return The name of trace extension
      */
-    virtual octf::table::Table buildExtension() = 0;
+    virtual const std::string &getName() const = 0;
 
-    virtual const google::protobuf::Message &handleIO(
-            const proto::trace::ParsedEvent &io) = 0;
+    /**
+     * @brief Gets the prototype of message of the trace extension
+     */
+    virtual MessageShRef getExtensionMessagePrototype() = 0;
 
-    // Builds empty message of the same type as message in handleIO
-    virtual const google::protobuf::Message &GetMessage() = 0;
+    /**
+     * @brief The trace extension building procedure step to handle trace event
+     *
+     * The extension builder might required a procedure which parses the trace a
+     * couple of times. This is handler for the step to handle the trace.
+     *
+     * @retval true Continue trace parsing
+     * @retval false Stop trace parsing, the step finished its job
+     */
+    typedef std::function<bool(const TraceEvent &event)> BuildStepEventHandler;
+
+    /**
+     * @brief Gets the build process description defined by the list of steps
+     * to execute.
+     *
+     * @return The List of steps for building extensions
+     */
+    virtual const std::list<BuildStepEventHandler> &getBuildProcess() = 0;
+
+    /**
+     * @brief Checks it the trace extension is ready for given trace
+     *
+     * @return true The trace extension is ready and can be saved respectively
+     * @return false The trace extension not ready, further processing needed
+     */
+    virtual bool isTraceExtensionReady() = 0;
+
+    /**
+     * @brief Gets the trace extension for the specific trace event
+     *
+     * @return The trace extension
+     */
+    virtual const google::protobuf::Message &getTraceExtension() = 0;
 };
 
 }  //  namespace octf
 
-#endif  // SOURCE_OCTF_INTERFACE_ITRACEXTENSIONBUILDER_H
+#endif  // SOURCE_OCTF_INTERFACE_ITRACEEXTENSIONBUILDER_H
